1bef2439793f450cfcb4d7f9ca45a23fa29e2890
Cosimo Lupo <clupo@google.com>
2020-03-09 13:07:42 +0000

    only require cffi, milksnake is only a build requirement

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(
     zip_safe=False,
     platforms="any",
     setup_requires=["milksnake"],
-    install_requires=["milksnake"],
+    install_requires=["cffi"],
     milksnake_tasks=[build_native],
 )
